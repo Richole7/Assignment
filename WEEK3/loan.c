85a53d1bc407c0c99377f2eeeca90f609d300f2b
Richole7 <richoleoduor@gmail.com>
2024-10-02 18:45:19 +0300

    Add files via upload

---
+++ WEEK3/loan.c
@@ -0,0 +1,27 @@
+//program to calculate loan
+/*
+ Author:Richole Oduor
+ Reg No : CT101/G/25060/24
+ Date: 30/09/2024
+ */
+//Preprocessor Directive
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int age, income;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your annual income: ");
+    scanf("%d", &income);
+
+    if (age >= 21 && income >= 21000) {
+        printf("Congratulations! You qualify for a loan.\n");
+    } else {
+        printf("Unfortunately, we are unable to offer you a loan at this time.\n");
+    }
+
+    return 0;
+}
